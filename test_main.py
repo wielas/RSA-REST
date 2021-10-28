0766c552aa537259bc694c6ad5fe6e5b4be4181e
wielas <wielickikrzysiu@gmail.com>
2021-10-28 17:19:52 +0200

    Tests for endpoits and cipher

---
+++ test_main.py
@@ -0,0 +1,39 @@
+import base64
+from fastapi.testclient import TestClient
+
+from main import app, rsa
+
+client = TestClient(app)
+test_str = "Aa.  12!*$^"
+valid_credentials = base64.b64encode(b"wielas:1234").decode("utf-8")
+
+
+def test_cipher():
+    encrypted_str = rsa.encrypt(test_str)
+    assert encrypted_str == rsa.encrypt(''.join(rsa.decrypt(encrypted_str)))
+
+
+def test_root():
+    """ Test root endpoint """
+    response = client.get("/", headers={"Authorization": "Basic " + valid_credentials})
+
+    assert response.status_code == 200
+    assert response.json()["username"] == "wielas"
+
+
+def test_encryption():
+    """ Test /encryption endpoint """
+    response = client.get(f"/encrypt/{test_str}", headers={"Authorization": "Basic " + valid_credentials})
+
+    assert response.status_code == 200
+    assert response.json()["encrypted message"] == rsa.encrypt(test_str)
+
+
+def test_decryption():
+    """ Test /decryption endpoint """
+    encrypted_str = rsa.encrypt(test_str)
+
+    response = client.get(f"/decrypt/{encrypted_str}", headers={"Authorization": "Basic " + valid_credentials})
+
+    assert response.status_code == 200
+    assert response.json()["decrypted message"] == ''.join(rsa.decrypt(encrypted_str))
